36575d50e2b9b08bff74d1bd47210d806d756b77
Gabriel Ramos <gabrielramos791@gmail.com>
2021-08-01 18:50:19 -0300

    updated views and urls to GET, POST, PUT and DELETE by REST

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list, name='data'),
+    path('', views.list, name='Overview'),
+    path('new-client/', views.create, name='New Client'),
+    path('update-client/<str:id>/', views.update, name='Update Client'),
+    path('delete-client/<str:id>/', views.delete, name='Delete Client'),
 ]
--- api/views.py
+++ api/views.py
@@ -3,10 +3,13 @@ from inspect import cleandoc
 from django.shortcuts import render
 
 from rest_framework.decorators import api_view
+from rest_framework.fields import REGEX_TYPE
 from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import ClientSerializer
 from .models import Client
+from api import serializers
 
 @api_view(['GET'])
 def list(request):
@@ -19,27 +22,33 @@ def list(request):
     clients = Client.objects.all()
     clients_serializer = ClientSerializer(clients, many=True)
 
-    #return Response({'overview': api_urls, 'data': client_serializer.data})
-    return Response(clients_serializer.data)
+    return Response({'overview': api_urls, 'data': clients_serializer.data})
 
-# @api_view(['POST'])
-# def create(request):
-#     api_urls = {
-#         'List': '/',
-#         'Create': '/new-client/',
-#         'Update': '/update-client/<str: key>',
-#         'Delete': '/delete-client/<str: key>'
-#     }
 
-#     return Response(api_urls)
+@api_view(['POST'])
+def create(request):
+    client_serializer = ClientSerializer(data=request.data)
 
-# @api_view(['POST'])
-# def update(request):
-#     api_urls = {
-#         'List': '/',
-#         'Create': '/new-client/',
-#         'Update': '/update-client/<str: key>',
-#         'Delete': '/delete-client/<str: key>'
-#     }
+    if not client_serializer.is_valid(): return Response({'Response': 'NOT AVAILABLE'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    
+    client_serializer.save()
+    return Response(client_serializer.data)
 
-#     return Response(api_urls)
+
+@api_view(['POST'])
+def update(request, id):
+    client = Client.objects.get(id=id)
+    client_serializer = ClientSerializer(instance=client, data=request.data)
+
+    if not client_serializer.is_valid(): return Response({"Response": "NOT FOUND"}, status=status.HTTP_404_NOT_FOUND)
+
+    client_serializer.save()
+    return Response(client_serializer.data)
+
+
+@api_view(['DELETE'])
+def delete(request, id):
+    client = Client.objects.get(id=id)
+    client.delete()
+
+    return Response({'Response': 'DELETED'}, status=status.HTTP_200_OK)
