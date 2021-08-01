db1ae8da6529cfdb9c6cfcafd2682135cb5f7472
Gabriel Ramos <gabrielramos791@gmail.com>
2021-08-01 18:03:45 -0300

    listing all clients on database

--- api/views.py
+++ api/views.py
@@ -1,3 +1,45 @@
+from http.client import TOO_MANY_REQUESTS
+from inspect import cleandoc
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import ClientSerializer
+from .models import Client
+
+@api_view(['GET'])
+def list(request):
+    api_urls = {
+        'Create': '/new-client/',
+        'Update': '/update-client/<str: key>',
+        'Delete': '/delete-client/<str: key>'
+    }
+
+    clients = Client.objects.all()
+    clients_serializer = ClientSerializer(clients, many=True)
+
+    #return Response({'overview': api_urls, 'data': client_serializer.data})
+    return Response(clients_serializer.data)
+
+# @api_view(['POST'])
+# def create(request):
+#     api_urls = {
+#         'List': '/',
+#         'Create': '/new-client/',
+#         'Update': '/update-client/<str: key>',
+#         'Delete': '/delete-client/<str: key>'
+#     }
+
+#     return Response(api_urls)
+
+# @api_view(['POST'])
+# def update(request):
+#     api_urls = {
+#         'List': '/',
+#         'Create': '/new-client/',
+#         'Update': '/update-client/<str: key>',
+#         'Delete': '/delete-client/<str: key>'
+#     }
+
+#     return Response(api_urls)
