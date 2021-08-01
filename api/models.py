e939d5fea2f3c312e0df3aa5c8446ce07c069167
Gabriel Ramos <gabrielramos791@gmail.com>
2021-08-01 17:02:21 -0300

    created client model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+class Client(models.Model):
+    STAGES_AVAILABLE = (
+        ('Documents', 'Aguardando assinatura de documentos'),
+        ('Resources', 'Aguardando transferência de recursos'),
+        ('Management', 'Gestão de patrimônio ativa'),
+    )
+
+    name = models.CharField(max_length=255, blank=False)
+    cpf = models.CharField(max_length=11, blank=False, unique=True)
+    stage = CharField(max_length=255, choices= STAGES_AVAILABLE)
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = 'Client'
+        verbose_name_plural = 'Clients'
